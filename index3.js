e4eef0ec067300c58e98bc4d3bdd8b842c70c0ed
Aniket1118 <91197235+Aniket1118@users.noreply.github.com>
2021-12-27 14:37:40 +0530

    first commit

---
+++ index3.js
@@ -0,0 +1,18 @@
+//create an array using .push with prompt and also using parseInt()
+var arr = [];
+arr.push(1,2,3,5)
+
+console.log(arr)
+
+/*
+ Take three numbers and find max. (Using arrays)
+var arr = (prompt("enter your elements"));
+arr = arr.split(',');                  //this splits string into an array 
+console.log(arr);
+var largest = 0; 
+for(i=0;i<largest;i=i+1){
+ if(arr[i]>largest){
+    var largest = arr[i];
+    console.log(largest);
+ }
+}*/
